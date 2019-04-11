53a91f6eca15adc918e2da6baea678ef47ec5590
hassanabedi <habedi11100@gmail.com>
2019-04-11 16:44:18 +0430

    add product single page

---
+++ template-parts/product/product-item3.php
@@ -0,0 +1,15 @@
+<!-- start item   -->
+        <div class="col-3 my-2"  >
+          <div class="dg-segustion-item   d-flex flex-column flex-nowrap hover-bg-3 p-0 pb-3 align-items-center ">
+
+              <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="img-fluid" alt="">
+              <div class="text-center p-2">
+                   <?php the_title(); ?>
+              </div>
+              <a href="<?php the_permalink(); ?>" class="btn bg-1 hover-bg-4 p2 col-5  ">
+                سفارش محصول
+              </a>
+
+          </div>
+        </div>
+        <!-- end item  -->
---
+++ template-parts/product/product-singlepage.php
@@ -0,0 +1,26 @@
+
+<!-- start single product part  -->
+<section class="container-fluid d-flex flex-row justify-content-between bg-3 mt-3">
+
+  <div class="col-5">
+
+    <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="img-fluid" alt="">
+
+  </div>
+  <div class="col-7 align-self-center text-right">
+
+    <h3>
+          <?php the_title(); ?>
+    </h3>
+    <h4>
+          شماره تماس
+    </h4>
+    <div>
+        <?php the_content(); ?>
+    </div>
+
+
+  </div>
+
+</section>
+<!--  end single product part -->
