84a79ea2895bf0c3298b70666045bff005bc612f
hassanabedi <habedi11100@gmail.com>
2019-04-17 09:53:50 +0430

    add archive page

---
+++ archive.php
@@ -0,0 +1,40 @@
+<?php
+get_header();
+
+
+
+?>
+
+<!--  start  product page  -->
+      <section class="container-fluid d-flex flex-row justify-content-between bg-3 mt-3">
+
+        <div class="col-3">
+
+
+        </div>
+        <div class="col-9 d-flex flex-wrap juastify-content-start">
+
+<?php
+
+if(have_posts()):
+
+  while(have_posts()):
+
+          the_post();
+
+          get_template_part("template-parts/weblog/weblog","weblog4");
+
+  endwhile;
+  wp_reset_postdata();
+else:
+  echo "پستی یافت نشد";
+
+endif;
+      ?>
+    </div>
+  </section>
+
+      <?php
+
+get_footer();
+ ?>
