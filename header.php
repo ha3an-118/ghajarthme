f75ada61cbedd30a5fb8d3c31185316c2ee2609a
hassanabedi <habedi11100@gmail.com>
2019-04-21 16:52:34 +0430

    add viewport

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html lang="fa" dir="rtl">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 
 
