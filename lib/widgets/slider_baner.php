01447e4b99f2999e6bf047627e91a173ccd9e220
hassanabedi <habedi11100@gmail.com>
2019-06-03 19:36:36 +0430

    solve div bug

--- lib/widgets/slider_baner.php
+++ lib/widgets/slider_baner.php
@@ -22,146 +22,129 @@
     public function widget($args, $instance)
     {
 
-      ?>
-    <!--         start baner part                   -->
-    <section class="contianer-fluid d-flex flex-md-row flex-column mt-2 dg-baner dir-ltr px-0 px-md-3">
+            ?>
+          <!--         start baner part                   -->
+      <section class="contianer-fluid d-flex flex-md-row flex-column mt-2 dg-baner dir-ltr px-0 px-md-3">
 
-      <div class="col-12 col-md-9 ">
-        <?php
-        $id = esc_attr($this->get_field_id("topbanerslider"));
-        self::$numberofslide =(empty($instance['numberofslide']))?3:$instance['numberofslide'];
-        global $index;
-
-        ?>
-
-        <div id="<?php echo $id; ?>" class="carousel slide">
-              <ol class="carousel-indicators">
-                <?php for( $index=0 ; $index< (int)self::$numberofslide ;$index++ ): ?>
-                      <li data-target="#<?php echo $id; ?>" data-slide-to="<?php echo $index ?>" class="<?php if($index==0){ echo "active";} ?>"></li>
-                <?php endfor; ?>
-              </ol>
-
-
-              <div class="carousel-inner">
-
-                <div >
+            <div class="col-12 col-md-9 ">
                   <?php
-                  for( $index=0 ; $index< (int)self::$numberofslide ;$index++ ){
-                        $mustshowpage = new WP_Query(array(
-                                                      'page_id' => (int)$instance['pageid'.$index],
-                                                    ));
+                  $id = esc_attr($this->get_field_id("topbanerslider"));
+                  self::$numberofslide =(empty($instance['numberofslide']))?3:$instance['numberofslide'];
+                  global $index;
+                  ?>
 
-                        if($mustshowpage->have_posts()):
-                          while ($mustshowpage->have_posts()) :
+                  <div id="<?php echo $id; ?>" class="carousel slide">
 
-                              $mustshowpage->the_post();?>
-                              <?php get_template_part("template-parts/widget/widget","slideritem"); ?>
+                          <ol class="carousel-indicators">
+                            <?php for( $index=0 ; $index< (int)self::$numberofslide ;$index++ ): ?>
+                                  <li data-target="#<?php echo $id; ?>" data-slide-to="<?php echo $index ?>" class="<?php if($index==0){ echo "active";} ?>"></li>
+                            <?php endfor; ?>
+                          </ol>
 
-                         <?php
-                          endwhile;//end while
-                       endif;//end if
-                       wp_reset_postdata();
+                          <div class="carousel-inner">
 
-                     }//end of for
-              ?>
+                                  <?php
+                                  for( $index=0 ; $index< (int)self::$numberofslide ;$index++ ){
+                                        $mustshowpage = new WP_Query(array(
+                                                                      'page_id' => (int)$instance['pageid'.$index],
+                                                                    ));
 
-              </div>
+                                        if($mustshowpage->have_posts()):
+                                          while ($mustshowpage->have_posts()) :
+
+                                              $mustshowpage->the_post();?>
+                                              <?php get_template_part("template-parts/widget/widget","slideritem"); ?>
+
+                                         <?php
+                                          endwhile;//end while
+                                       endif;//end if
+                                       wp_reset_postdata();
+
+                                     }//end of for
+                                  ?>
+                          </div> <!-- end of .carousel-inner  -->
+
+                          <a class="carousel-control-prev" href="#<?php echo $id; ?>" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                          </a>
+                          <a class="carousel-control-next" href="#<?php echo $id; ?>" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                          </a>
+                  </div>
+
+            </div> <!--  end fo .col9 -->
+
+            <div class="col-12 col-sm-10 mx-sm-auto col-md-3 d-md-flex justify-content-center d-none ">
+
+                  <?php
+                  $baner = new WP_Query(array(
+                                                'page_id' => (int)$instance['baner'],
+                                              ));
+                  if($baner->have_posts()):
+                    while($baner->have_posts()):
+                      $baner->the_post();
+                      ?>
+                      <a href="<?php the_permalink(); ?>" style="height:100%;">
+
+                        <img class="img-fluid " src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+
+                      </a>
 
 
-              <a class="carousel-control-prev" href="#<?php echo $id; ?>" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="carousel-control-next" href="#<?php echo $id; ?>" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
+           </div>
+           <div id="habannerpupup" class="ha-baner-pupup"
+              beforewidth="0" beforeheight="0" beforepositiontop="0" beforepositionleft="0"
+              afterwidth="100%" afterheight="100%" afterpositiontop="0" afterpositionleft="0" >
+
+                  <button expandwinid="habannerpupup" class="btn bg-trnsparent" >
+                    <i class="fa fa-times fa-2x text-1"></i>
+                  </button>
+                  <div class="col-9 mx-auto">
+                      <a href="<?php the_permalink(); ?>" style="height:100%;">
+
+                        <img class="img-fluid " src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+
+                      </a>
+                  </div>
           </div>
 
+          <div class="fixed-bottom  d-md-none alert alert-warning alert-dismissible fade show" role="alert">
 
-      </div> <!--  end fo .col9 -->
+                  <button expandwinid="habannerpupup" class="btn bg-trnsparent" >
+                      <strong class="text-danger">یک پیشنهاد ویژه دارید </strong> برای نمایش کلید کنید.
+                  </button>
 
-<div class="col-12 col-sm-10 mx-sm-auto col-md-3 d-md-flex justify-content-center d-none ">
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                  </button>
+          </div>
 
+      <?php
+            endwhile;
+        endif;
+        wp_reset_postdata();
 
-  <!-- <img class="img-fluid " src="pic/3.jpg" alt=""> -->
-  <?php // TODO: must get page from admin ui and show heare ?>
-  <?php
-  $baner = new WP_Query(array(
-                                'page_id' => (int)$instance['baner'],
-                              ));
-  if($baner->have_posts()):
-    while($baner->have_posts()):
-      $baner->the_post();
       ?>
-      <a href="<?php the_permalink(); ?>" style="height:100%;">
+      <script type="text/javascript">
+            $('[data-dismiss=alert]').click(function(){
 
-        <img class="img-fluid " src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+              $(this).parents('[role=alert]').hide();
 
-      </a>
+            });
 
-
-</div>
-<div id="habannerpupup" class="ha-baner-pupup"
-  beforewidth="0" beforeheight="0" beforepositiontop="0" beforepositionleft="0"
-  afterwidth="100%" afterheight="100%" afterpositiontop="0" afterpositionleft="0" >
-
-    <button expandwinid="habannerpupup" class="btn bg-trnsparent" >
-      <i class="fa fa-times fa-2x text-1"></i>
-    </button>
-  <div class="col-9 mx-auto">
-    <a href="<?php the_permalink(); ?>" style="height:100%;">
+            $(document).ready(function(){
+              $('#<?php echo $id ?>').carousel({
+                  interval: <?php echo (int)$instance["timeofslide"]*1000 ?>,
+                })
+            });
 
 
-      <img class="img-fluid " src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+      </script>
 
-    </a>
-
-  </div>
-
-
-
-</div>
-
-
-
-
-  <div class="fixed-bottom  d-md-none alert alert-warning alert-dismissible fade show" role="alert">
-
-    <button expandwinid="habannerpupup" class="btn bg-trnsparent" >
-        <strong class="text-danger">یک پیشنهاد ویژه دارید </strong> برای نمایش کلید کنید.
-    </button>
-
-
-
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>
-
-<?php
-      endwhile;
-  endif;
-  wp_reset_postdata();
-
-?>
-<script type="text/javascript">
-      $('[data-dismiss=alert]').click(function(){
-
-        $(this).parents('[role=alert]').hide();
-
-      });
-
-      $(document).ready(function(){
-        $('#<?php echo $id ?>').carousel({
-            interval: <?php echo (int)$instance["timeofslide"]*1000 ?>,
-          })
-      });
-
-
-</script>
-</section> <!-- end of .dg-baner -->
-    <?php
+      </section> <!-- end of .dg-baner -->
+          <?php
 
 
   } // end of widget function
