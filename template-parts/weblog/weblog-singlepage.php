dfb8efd2ba83f5a067015ae0637b24d72441635e
hassanabedi <habedi11100@gmail.com>
2019-06-07 19:43:50 +0430

    add body max-width for chrome solve

--- template-parts/weblog/weblog-singlepage.php
+++ template-parts/weblog/weblog-singlepage.php
@@ -3,7 +3,7 @@
 
   <div class="col-12 col-md-10 mx-auto">
 
-    <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="img-fluid mx-auto" alt="">
+    <img src="<?php echo get_the_post_thumbnail_url(); ?>" class="img-fluid mx-auto d-block" alt="">
 
   </div>
   <article class="text-right">
